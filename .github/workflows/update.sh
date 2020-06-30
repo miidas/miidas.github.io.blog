e1f4b926e43908c35ae5c4f5dad584ede13e0c06
miidas <67558797+miidas0@users.noreply.github.com>
2020-06-30 12:15:56 +0900

    Update update.sh

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo ${_GITHUB_ACCESS_TOKEN_}
 git config --global user.name "miidas"
 git config --global user.email "67558797+miidas0@users.noreply.github.com"
-
 git clone --recursive https://${GITHUB_ACTOR}:${_GITHUB_ACCESS_TOKEN_}@github.com/${GITHUB_ACTOR}/${GITHUB_ACTOR}.github.io.git
 cd ${GITHUB_ACTOR}.github.io
 cd blog
