1c58fa3521740cdf6f2768c250eaafc242cc41f1
miidas <67558797+miidas0@users.noreply.github.com>
2020-06-30 12:14:51 +0900

    Update update.sh

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -5,7 +5,6 @@ git config --global user.email "67558797+miidas0@users.noreply.github.com"
 
 git clone --recursive https://${GITHUB_ACTOR}:${_GITHUB_ACCESS_TOKEN_}@github.com/${GITHUB_ACTOR}/${GITHUB_ACTOR}.github.io.git
 cd ${GITHUB_ACTOR}.github.io
-#git remote set-url origin https://${GITHUB_ACTOR}:${_GITHUB_ACCESS_TOKEN_}@github.com/${GITHUB_ACTOR}/${GITHUB_ACTOR}.github.io.git
 cd blog
 git checkout master
 cd ../
